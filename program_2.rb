d4693fe119a8db9d294a780e90ab6c240ba10d97
jeliasrm <jesus.eliot.rm@gmail.com>
2016-08-22 12:54:53 -0600

    Sentece when

--- program_2.rb
+++ program_2.rb
@@ -1,13 +1,13 @@
 age = 3
-if age >= 0 && age <=2
-	puts "bebe"
-elsif age >= 3 && age <= 12
-	puts "niño"
-elsif age >= 13 && age <= 17
-	puts "adolescente"
-else
-	puts "adulto"
-end
+#if age >= 0 && age <=2
+#	puts "bebe"
+#elsif age >= 3 && age <= 12
+#	puts "niño"
+#elsif age >= 13 && age <= 17
+#	puts "adolescente"
+#else
+#	puts "adulto"
+#end
 
 case age
 when 0..2
