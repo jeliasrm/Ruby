b7e3dcf25a878274a88a68bb3418ff1222bef69e
jeliasrm <jesus.eliot.rm@gmail.com>
2016-08-22 14:20:05 -0600

    Program_3

--- program_2.rb
+++ program_2.rb
@@ -9,13 +9,46 @@ age = 3
 #	puts "adulto"
 #end
 
-case age
-when 0..2
-	puts "bebe"
-when 3..12
-	puts "niño"
-when 13..17
-	puts "adolescente"
-else
-	puts "adulto"
+def classify(age)
+	case age
+	when 0..2
+		puts "bebe"
+	when 3..12
+		puts "niño"
+	when 13..17
+		puts "adolescente"
+	else
+		puts "adulto"
+	end
+end
+
+puts classify(30)
+
+x = 0
+numero = 5
+
+while x < 5
+ puts "hola estoy en el while #{x}"
+ x += 1
+end
+
+puts "fin"
+
+x = 0
+until x == 5
+ puts "hola estoy en until #{x}"
+ x += 1
+end
+
+x = 0
+loop do
+ puts "hola estoy dentro del loop #{x}"
+ x += 1
+ break if x == 5
+end
+
+x = 0
+
+for x in (0..4)
+ puts "hola estoy en el for #{x}"
 end
---
+++ program_3.rb
@@ -0,0 +1,18 @@
+puts "ruby es divertido".capitalize
+puts "RAQUEL@GMAIL.COM".downcase
+puts "RUBY es DIVERTIDO".swapcase
+puts "alumnos de platzi".insert(0,"Hola ")
+puts "raquel hernandez".reverse
+
+puts "ruby es divretido divretido".gsub('divretido','divertido')
+puts "ruby es divretido divretido".sub('divretido','divertido')
+puts "alumnos de platzi".gsub('a','@')
+puts "  alumnos de platzi   ".strip
+
+puts "alumnos de platzi".include?("platzi")
+
+puts "alumnos de platzi".start_with?("al")
+
+
+
+puts "a".next
